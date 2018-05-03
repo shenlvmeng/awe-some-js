93a4ac1890df289bad7d9dd5478495b95ae1a752
xinyuanming <xinyuanming@bytedance.com>
2018-05-03 17:57:49 +0800

    feat: add commonjs
    Change-Id: Ib44a578fc3e29a8972716757cf618e59f947430c

--- src/base.js
+++ src/base.js
@@ -1,8 +1,20 @@
 const noop = () => {};
-const classNames = (obj) => Object.keys(obj).filter((key) => obj[key]).join(' ');
 
+const classNames = (...names) => {
+    function convert(name) {
+        return isObject(name) ?
+            Object.keys(name).filter((key) => name[key]).join(' ')
+            : (name || '');
+    }
+    return names.map(name => convert(name)).join(' ');
+}
+
+const commonJS = (require, exports, module, code) => {
+    (new Function('require', 'exports', 'module', code)).call(exports, require, exports, module);
+}
 
 export default {
     noop,
     classNames,
+    commonJS,
 }
