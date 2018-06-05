288f673d08d7492d15765bac4a35efc1b3bbfb37
xinyuanming <xinyuanming@bytedance.com>
2018-06-05 14:05:48 +0800

    feat: 新增lodash/get
    Change-Id: Ie899e05e474e1c189fac7d9b62679b6808c08f31

--- src/base.js
+++ src/base.js
@@ -1,4 +1,21 @@
+import { isUndefined } from './is';
+
 const noop = () => {};
+
+const get = (obj, path, default) => {
+    const keys = path.split('.');
+    let res = obj;
+    // 一个可以提前跳出的forEach
+    keys.every(key => {
+        if (isUndefined(res.key)) {
+            res = default || false;
+            return false;
+        }
+        res = res.key;
+        return true;
+    });
+    return res;
+}
 
 const classNames = (...names) => {
     function convert(name) {
